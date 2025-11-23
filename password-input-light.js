df75fdb975a6dabf29a71d793c192062c05799d9
zayennn <zaayeennn@gmail.com>
2025-11-23 22:45:25 +0700

    update

--- password-input-light.js
+++ password-input-light.js
@@ -5,7 +5,7 @@ const passwordInput = document.getElementById('password');
 
 root.addEventListener('mousemove', (e) => {
   let rect = beam.getBoundingClientRect();
-  let mouseX = rect.right + (rect.width / 2); 
+  let mouseX = rect.right + (rect.width / 2);
   let mouseY = rect.top + (rect.height / 2);
   let rad = Math.atan2(mouseX - e.pageX, mouseY - e.pageY);
   let degrees = (rad * (20 / Math.PI) * -1) - 350;
